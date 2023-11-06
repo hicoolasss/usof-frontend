5efd570ad6595bd071c9100dd201588fb7684058
hicoolasss <criopsdaun@gmail.com>
2023-11-06 17:03:30 +0200

    whit bg fix

--- components/lightning-bulb.jsx
+++ components/lightning-bulb.jsx
@@ -4,6 +4,6 @@ import Spline from '@splinetool/react-spline';
 
 export default function Scene() {
   return (
-    <Spline className='cursor-pointer bg-white ' scene="https://prod.spline.design/I4R3sPrR89R1mvlf/scene.splinecode" />
+    <Spline className='cursor-pointer bg-gradient ' scene="https://prod.spline.design/I4R3sPrR89R1mvlf/scene.splinecode" />
   );
 }
